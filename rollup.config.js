958bbad032339970dbb2e980679a9eba548c6696
gstrada <gianlucaxstrada@gmail.com>
2022-08-08 16:04:34 +0200

    chore (rollup): simplified build scripts with rollup configuration, bundle refactor with BS css and web elements -> popper + BS.js to be imported in Universal

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,17 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+//TODO resolve process.env.NODE_ENV
 import summary from 'rollup-plugin-summary';
 import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import { copy } from '@web/rollup-plugin-copy';
 import typescript from 'rollup-plugin-typescript2';
+import scss from "rollup-plugin-scss";
+import postcss from "postcss";
+import autoprefixer from "autoprefixer";
+import * as path from "path";
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 console.log('-->', process.env.NODE_ENV);
@@ -33,25 +38,40 @@ export default {
     // Resolve bare module specifiers to relative paths
     resolve({browser: true}),
     // Minify JS
-    terser({
-      ecma: 2020,
-      mangle: {toplevel: true},
-      compress: {
-        module: true,
-        toplevel: true,
-        unsafe_arrows: true,
-        // drop_console: !devMode,
-        // drop_debugger: !devMode
-      },
-      output: {quote_style: 1}
-    }),
+    // terser({
+    //   ecma: 2020,
+    //   mangle: {toplevel: true},
+    //   compress: {
+    //     module: true,
+    //     toplevel: true,
+    //     unsafe_arrows: true,
+    //     // drop_console: !devMode,
+    //     // drop_debugger: !devMode
+    //   },
+    //   output: {quote_style: 1}
+    // }),
     typescript({
       useTsconfigDeclarationDir: true,
       tsconfigOverride: {
         //base component is useful only to create new components, not in the bundle
         exclude: ['src/components/base-component.ts/'],
       },
-    }),
+    },
+      scss({
+        processor: () => postcss([autoprefixer()]),
+        includePaths: [
+          path.resolve('node_modules')
+        ],
+        //minify
+        outputStyle: 'compressed',
+        sourceMap: true,
+        extract: true,
+        extensions: ['.sass','.css'],
+        // Prefix global scss. Useful for variables and mixins.
+        prefix: `@import "./public/theme/public.scss";`,
+        output: "./dist/css/style.css",
+        failOnError: true,
+      })),
     // Print bundle summary
     summary(),
     // Optional: copy any static assets to build directory
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,3 @@ export * from './components/card';
 
 //dependencies
 export * from './utility/Utility';
-export * from '@popperjs/core'
-export * from 'bootstrap'
