2674293ecd434285aaa357616e5dd677274c4cd5
gstrada <gianlucaxstrada@gmail.com>
2022-08-08 16:59:37 +0200

    fix (build): configuration for different environment

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-//TODO resolve process.env.NODE_ENV
 import summary from 'rollup-plugin-summary';
 import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
@@ -16,10 +15,59 @@ import postcss from "postcss";
 import autoprefixer from "autoprefixer";
 import * as path from "path";
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-console.log('-->', process.env.NODE_ENV);
-const devMode = (process.env.NODE_ENV === 'development');
-console.log(`${devMode ? 'development' : 'production'} mode bundle`);
+console.log('ENV: ', process.env.NODE_ENV);
+const devMode = (process.env.NODE_ENV === 'dev');
+
+let _plugins = [
+  replace({'Reflect.decorate': 'undefined'}),
+  // Resolve bare module specifiers to relative paths
+  resolve({browser: true}),
+  typescript({
+      useTsconfigDeclarationDir: true,
+      tsconfigOverride: {
+        //base component is useful only to create new components, not in the bundle
+        exclude: ['src/components/base-component.ts/'],
+      },
+    },
+    scss({
+      processor: () => postcss([autoprefixer()]),
+      includePaths: [
+        path.resolve('node_modules')
+      ],
+      //minify
+      outputStyle: 'compressed',
+      sourceMap: true,
+      extract: true,
+      extensions: ['.sass','.css'],
+      // Prefix global scss. Useful for variables and mixins.
+      prefix: `@import "./public/theme/public.scss";`,
+      output: "./dist/css/style.css",
+      failOnError: true,
+    })),
+  // Print bundle summary
+  summary(),
+  // Optional: copy any static assets to build directory
+  copy({
+    //patterns: ['images/**/*'],
+  }),
+];
+
+if (!devMode) {
+  _plugins.push(
+    // Minify JS
+    terser({
+      ecma: 2020,
+      mangle: {toplevel: true},
+      compress: {
+        module: true,
+        toplevel: true,
+        unsafe_arrows: true,
+        drop_console: !devMode,
+        drop_debugger: !devMode
+      },
+      output: {quote_style: 1}
+    }))
+}
 
 export default {
   input: './src/main.ts',
@@ -33,50 +81,5 @@ export default {
       console.error(`(!) ${warning.message}`);
     }
   },
-  plugins: [
-    replace({'Reflect.decorate': 'undefined'}),
-    // Resolve bare module specifiers to relative paths
-    resolve({browser: true}),
-    // Minify JS
-    // terser({
-    //   ecma: 2020,
-    //   mangle: {toplevel: true},
-    //   compress: {
-    //     module: true,
-    //     toplevel: true,
-    //     unsafe_arrows: true,
-    //     // drop_console: !devMode,
-    //     // drop_debugger: !devMode
-    //   },
-    //   output: {quote_style: 1}
-    // }),
-    typescript({
-      useTsconfigDeclarationDir: true,
-      tsconfigOverride: {
-        //base component is useful only to create new components, not in the bundle
-        exclude: ['src/components/base-component.ts/'],
-      },
-    },
-      scss({
-        processor: () => postcss([autoprefixer()]),
-        includePaths: [
-          path.resolve('node_modules')
-        ],
-        //minify
-        outputStyle: 'compressed',
-        sourceMap: true,
-        extract: true,
-        extensions: ['.sass','.css'],
-        // Prefix global scss. Useful for variables and mixins.
-        prefix: `@import "./public/theme/public.scss";`,
-        output: "./dist/css/style.css",
-        failOnError: true,
-      })),
-    // Print bundle summary
-    summary(),
-    // Optional: copy any static assets to build directory
-    copy({
-      //patterns: ['images/**/*'],
-    }),
-  ],
+  plugins: _plugins,
 };
